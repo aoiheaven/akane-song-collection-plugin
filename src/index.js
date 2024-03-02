939231036e8af34e61b1e8f21aa134b15497ed4d
aoiheaven <devilqmcq@gmail.com>
2024-03-03 00:12:22 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './app'
+
+const wokooApp = document.createElement('div')
+wokooApp.id = 'wokooApp-akane-song-collection-plugin-17668'
+document.body.appendChild(wokooApp)
+ReactDOM.render(<App />, wokooApp)
