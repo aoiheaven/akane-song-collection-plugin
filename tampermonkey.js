8a0ba6f01466335d38c5bfe74b2ec1786e30c7e6
aoiheaven <devilqmcq@gmail.com>
2024-03-03 11:10:55 +0800

    Add eslint & prettier support

--- tampermonkey.js
+++ tampermonkey.js
@@ -9,10 +9,10 @@
 
 // ==/UserScript==
 
-;(function () {
-  'use strict'
-  if (location.href === 'http://localhost:8080/') return
-  var script = document.createElement('script')
-  script.src = 'http://localhost:8080/app.bundle.js'
-  document.body.appendChild(script)
-})()
+(function () {
+  "use strict";
+  if (location.href === "http://localhost:8080/") return;
+  var script = document.createElement("script");
+  script.src = "http://localhost:8080/app.bundle.js";
+  document.body.appendChild(script);
+})();
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,24 +1,27 @@
 module.exports = {
   entry: {
-    app: '/src/index.js',
+    app: "/src/index.js",
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/, //匹配js文件
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-react', '@babel/preset-env'],
+            presets: ["@babel/preset-react", "@babel/preset-env"],
             plugins: [
-              ['@babel/plugin-proposal-decorators', { legacy: true }],
-              ['@babel/plugin-proposal-class-properties', { loose: true }],
-              ['@babel/plugin-proposal-private-methods', { loose: true }],
-              ['@babel/plugin-proposal-private-property-in-object', { loose: true }]
+              ["@babel/plugin-proposal-decorators", { legacy: true }],
+              ["@babel/plugin-proposal-class-properties", { loose: true }],
+              ["@babel/plugin-proposal-private-methods", { loose: true }],
+              [
+                "@babel/plugin-proposal-private-property-in-object",
+                { loose: true },
+              ],
             ],
           },
         },
       },
     ],
   },
-}
+};
